4667db1f82f6950027a4a002b0e1051e4dd3820f
hoiru-futabaya <hoiru.hutabaya@gmail.com>
2020-07-05 21:00:08 +0900

    重複のこと考えてなかったので追加

--- main.py
+++ main.py
@@ -8,16 +8,23 @@ word = input('解読したい単語を入力: ')
 print('解読中...')
 
 #分割（重複は削除）
-separated = list(set(word))
-#print(separated)
+moji = list(set(word))
+separated = list(word)
 
-##何分割するか計算
-#length = len(separated)
-#print(str(length) + '分割')
+#何分割するか計算
+length = len(separated)
 
-#並べ替え
-for v in itertools.permutations(separated):
-    print(''.join(v))
+#重複ありで並べ替え結果を入れるリスト
+doublist = []
+
+#並べ替えてdoublistにどんどん入れる
+for v in itertools.permutations(separated, length):
+    doublist.append(''.join(v))
+
+#doublistの中の重複を除いて出力
+result = list(set(doublist))
+for l in range(len(result)):
+    print(result[l])
 
 
 
